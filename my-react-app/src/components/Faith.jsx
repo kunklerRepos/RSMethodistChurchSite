208b297ec7dc250f0a1ec6d41b8e2b5708f6108d
Skunkler <42595638+Skunkler@users.noreply.github.com>
2025-03-23 22:08:00 -0400

    faith post

---
+++ my-react-app/src/components/Faith.jsx
@@ -0,0 +1,251 @@
+import 'react' from React;
+
+
+function FaithConfession()
+{
+    return (
+        <>
+        <h4 id="Trinity">1. Faith in the Holy Trinity</h4>
+        <p>We believe in the one living and true God, both holy and loving, eternal, unlimited in power, wisdom,
+        and goodness, the Creator and Preserver of all things. Within this unity there are three persons of one
+        essential nature, power and eternity—the Father, the Son, and the Holy Spirit.</p>
+        
+        <h4 id="Father">2. The Father</h4>
+        <p>We believe the Father is the Source of all that exists, whether of matter or spirit. With the Son and the
+            Holy Spirit, He made man, male and female, in His image. By intention He relates to people as Father,
+            thereby forever declaring His goodwill toward them. In love, He both seeks and receives penitent
+            sinners.</p>    
+
+        <h4 id="Son">3. The Son</h4>
+        <p>We believe in Jesus Christ, the only begotten Son of God. He was conceived by the Holy Spirit and born
+        of the Virgin Mary, truly God and truly man. He died on the cross and was buried, to be a sacrifice both
+        for original sin and for all human transgressions, and to reconcile us to God. Christ rose bodily from the
+        dead, and ascended into heaven, and there intercedes for us at the Father’s right hand until He returns
+        to judge all humanity at the last day.</p>
+
+        <h4 id="HolySpirit">4. The Holy Spirit</h4>
+        <p>We believe in the Holy Spirit who proceeds from the Father and the Son, and is of the same essential
+        nature, majesty, and glory, as the Father and the Son, truly and eternally God. He is the Administrator of
+        grace to all, and is particularly the effective Agent in conviction for sin, in regeneration, in sanctification,
+        and in glorification. He is ever present, assuring, preserving, guiding, and enabling the believer.</p>
+
+        <h4 id="SolaFide">5. The Sufficiency and Full Authority of the Holy Scriptures for Salvation</h4>
+        <p>We believe that the books of the Old and New Testaments constitute the Holy Scriptures. They are the
+        inspired and infallibly written Word of God, fully inerrant in their original manuscripts and superior to all
+        human authority, and have been transmitted to the present without corruption of any essential
+        doctrine. We believe that they contain all things necessary to salvation; so that whatever is not read
+        therein, nor may be proved thereby, is not to be required of any man or woman that it should be
+        believed as an article of faith, or be thought requisite or necessary to salvation. Both in the Old and New
+        Testaments life is offered ultimately through Christ, who is the only Mediator between God and
+        humanity. The New Testament teaches Christians how to fulfill the moral principles of the Old
+        Testament, calling for loving obedience to God made possible by the indwelling presence of His Holy
+        Spirit.
+
+        The canonical books of the Old Testament are:
+        Genesis, Exodus, Leviticus, Numbers, Deuteronomy, Joshua, Judges, Ruth, 1 Samuel, 2 Samuel, 1 Kings, 2
+        Kings, 1 Chronicles, 2 Chronicles, Ezra, Nehemiah, Esther, Job, Psalms, Proverbs, Ecclesiastes, The Song
+
+        Page | 2
+
+        Articles of Religion and Beliefs – Rising Sun Methodist Church (2025)
+
+        of Solomon, Isaiah, Jeremiah, Lamentations, Ezekiel, Daniel, Hosea, Joel, Amos, Obadiah, Jonah, Micah,
+        Nahum, Habakkuk, Zephaniah, Haggai, Zechariah and Malachi.
+        The canonical books of the New Testament are:
+        Matthew, Mark, Luke, John, Acts, Romans, 1 Corinthians, 2 Corinthians, Galatians, Ephesians,
+        Philippians, Colossians, 1 Thessalonians, 2 Thessalonians, 1 Timothy, 2 Timothy, Titus, Philemon,
+        Hebrews, James, 1 Peter, 2 Peter, 1 John, 2 John, 3 John, Jude and Revelation.
+        </p>
+        
+        <h4 id="Purose">6. God’s Purpose for Humanity</h4>
+        <p>We believe that the two great commandments which require us to love the Lord our God with all the
+        heart, and our neighbors as ourselves, summarize the divine law as it is revealed in the Scriptures. They
+        are the perfect measure and norm of human duty, both for the ordering and directing of families and
+        nations, and all other social bodies, and for individual acts, by which we are required to acknowledge
+        God as our only Supreme Ruler, and all persons as created by Him, equal in all natural rights. Therefore
+        all persons should so order all their individual, social, and political acts as to give to God entire and
+        absolute obedience, and to assure to all the enjoyment of every natural right, as well as to promote the
+        fulfillment of each in the possession and exercise of such rights.</p>
+
+        <h4 id="Family">7. Marriage and the Family</h4>
+        <p>We believe that every person is created in the image of God, that human sexuality reflects that image in
+        terms of intimate love, communication, fellowship, subordination of the self to the larger whole, and
+        fulfillment. God’s Word makes use of the marriage relationship as the supreme metaphor for His
+        relationship with His covenant people and for revealing the truth that this relationship is of one God
+        with one people. Therefore God’s plan for human sexuality is that it is to be expressed only in a
+        monogamous lifelong relationship between one man and one woman within the framework of
+        marriage. This is the only relationship which is divinely designed for the birth and rearing of children and
+        is a covenant union made in the sight of God, taking priority over every other human relationship.</p>
+
+        <h4 id="PersonalChoice">8. Personal Choice</h4>
+        <p>
+        We believe that humanity’s creation in the image of God included ability to choose between right and
+        wrong. Thus individuals were made morally responsible for their choices. But since the fall of Adam,
+        people are unable in their own strength to do the right. This is due to original sin, which is not simply the
+        following of Adam’s example, but rather the corruption of the nature of each mortal, and is reproduced
+        naturally in Adam’s descendants. Because of it, humans are very far gone from original righteousness,
+        and by nature are continually inclined to evil. They cannot of themselves even call upon God or exercise
+        faith for salvation. But through Jesus Christ the prevenient grace of God makes possible what humans in
+        self effort cannot do. It is bestowed freely upon all, enabling all who will to turn and be saved.
+        </p>
+
+        <h4 id="Sin">9. Sin: Original, Willful, and Involuntary</h4>
+        <p>We believe that through the disobedience of Adam and Eve sin entered the world and all creation
+        suffered its consequences. The effects of sin include disruption of the relationship between God and
+        humanity, deterioration of the natural order of creation, and exploitation of persons by evil or
+        misguided social systems. The whole of creation groans for redemption. Each person is born with a
+        proclivity toward sin, manifested in an inordinate orientation toward self and independence from God,
+        leading to deliberate acts of unrighteousness. The residual effects of Adam and Eve’s disobedience
+        include a marred human nature from which arise involuntary shortcomings, faults, infirmities, and
+        imperfect judgments, which should not be accounted the same as willful sin. However, as
+        manifestations of the fallen nature of humanity, these shortcomings of God’s holiness still necessitate
+        the merits of the atonement, the sanctifying work of the Holy Spirit, and the self-control of the believer.
+        Willful sin results when a morally accountable person chooses to violate a known law of God, using
+        freedom of choice to please self rather than obey God. The consequences of willful sin include a loss of
+        fellowship with God, a self-absorption with one’s own interests rather than love and concern for others,
+        a bondage to things which distort the divine image, a persistent inability to live righteously, and
+        ultimately everlasting misery and separation from God. The atoning work of Christ is the only remedy for
+        sin, whether original, willful or involuntary.</p>
+
+        <h4 id="Atonement">10. The Atonement</h4>
+        <p>We believe that Christ’s offering of himself, once and for all, through His sufferings and meritorious
+            death on the cross, provides the perfect redemption and atonement for the sins of the whole world,
+            both original and actual. There is no other ground of salvation from sin but that alone. This atonement is
+            sufficient for every individual of Adam’s race. It is unconditionally effective in the salvation of those
+            mentally incompetent from birth, of those converted persons who have become mentally incompetent,
+            and of children under the age of accountability. But it is effective for the salvation of those who reach
+            the age of accountability only when they repent and exercise faith in Christ.</p>
+        
+        <h4 id="Repentance">11. Repentance and Faith</h4>
+        <p>We believe that for men and women to appropriate what God’s prevenient grace has made possible,
+        they must voluntarily respond in repentance and faith. The ability comes from God, but the act is the
+        individual’s.
+        Repentance is prompted by the convicting ministry of the Holy Spirit. It involves a willful change of mind
+        that renounces sin and longs for righteousness, a godly sorrow for and a confession of past sins, proper
+        restitution for wrongdoings, and a resolution to reform the life. Repentance is the precondition for
+        saving faith, and without it saving faith is impossible. Faith, in turn, is the only condition of salvation. It
+        begins in the agreement of the mind and the consent of the will to the truth of the gospel, but issues in
+        a complete reliance by the whole person in the saving ability of Jesus Christ and a complete trusting of
+        oneself to Him as Savior and Lord. Saving faith is expressed in a public acknowledgment of His Lordship
+        and an identification with His Church.</p>
+
+        <h4 id="Justification">12. Justification, Regeneration and Adoption</h4>
+        <p>
+        We believe that when one repents of personal sin and believes on the Lord Jesus Christ, that at the
+            same moment that person is justified, regenerated, adopted into the family of God, and assured of
+            personal salvation through the witness of the Holy Spirit.
+            We believe that justification is the judicial act of God whereby a person is accounted righteous, granted
+            full pardon of all sin, delivered from guilt, completely released from the penalty of sins committed, by
+            the merit of our Lord and Savior Jesus Christ, by faith alone, not on the basis of works.
+            We believe that regeneration, or the new birth, is that work of the Holy Spirit whereby, when one truly
+            repents and believes, one’s moral nature is given a distinctively spiritual life with the capacity for love
+            and obedience. This new life is received by faith in Jesus Christ, it enables the pardoned sinner to serve
+            God with the will and affections of the heart, and by it the regenerate are delivered from the power of
+            sin which reigns over all the unregenerate.
+            We believe that adoption is the act of God by which the justified and regenerated believer becomes a
+            partaker of all the rights, privileges, and responsibilities of a child of God.
+        </p>
+
+        <h4 id="GoodWorks">13. Good Works</h4>
+        <p>We believe that although good works cannot save us from our sins or from God’s judgment, they are the
+        fruit of faith and follow after regeneration. Therefore they are pleasing and acceptable to God in Christ,
+        and by them a living faith may be as evidently known as a tree is discerned by its fruit.</p>
+
+        <h4 id="Regeneration">14. Sin After Regeneration</h4>
+        <p>
+        We believe that after we have experienced regeneration, it is possible to fall into sin, for in this life there
+        is no such height or strength of holiness from which it is impossible to fall. But by the grace of God one
+        who has fallen into sin may by true repentance and faith find forgiveness and restoration.
+        </p>
+
+        <h4 id="Sanctification">15. Sanctification: Initial, Progressive, Entire</h4>
+        <p>We believe that sanctification is that work of the Holy Spirit by which the child of God is separated from
+        sin unto God and is enabled to love God with all the heart and to walk in all His holy commandments
+        blameless. Sanctification is initiated at the moment of justification and regeneration. From that moment
+        there is a gradual or progressive sanctification as the believer walks with God and daily grows in grace
+        and in a more perfect obedience to God. This prepares for the crisis of entire sanctification which is
+        wrought instantaneously when believers present themselves as living sacrifices, holy and acceptable to
+        God, through faith in Jesus Christ, being effected by the baptism with the Holy Spirit who cleanses the
+        heart from all inbred sin. The crisis of entire sanctification perfects the believer in love and empowers
+        that person for effective service. It is followed by lifelong growth in grace and the knowledge of our Lord
+        and Savior, Jesus Christ. The life of holiness continues through faith in the sanctifying blood of Christ and
+        evidences itself by loving obedience to God’s revealed will.</p>
+
+        <h4 id="Spirit">16. The Gifts of the Spirit</h4>
+        <p>
+        We believe that the Gift of the Spirit is the Holy Spirit himself, and He is to be desired more than the
+        gifts of the Spirit which He in His wise counsel bestows upon individual members of the Church to
+        enable them properly to fulfill their function as members of the body of Christ. The gifts of the Spirit,
+        although not always identifiable with natural abilities, function through them for the edification of the
+        whole Church. These gifts are to be exercised in love under the administration of the Lord of the Church,
+        not through human volition. The relative value of the gifts of the Spirit is to be tested by their usefulness
+        in the Church and not by the ecstasy produced in the ones receiving them. 
+        </p>
+
+        <h4 id="Church">17. The Church</h4>
+        <p>
+        We believe that the Christian Church is the entire body of believers in Jesus Christ, who is the founder
+        and only Head of the Church. The Church includes both those believers who have gone to be with the
+        Lord and those who remain on the earth, having renounced the world, the flesh, and the devil, and
+        having dedicated themselves to the work which Christ committed unto His church until He comes. The
+        Church on earth is to preach the pure Word of God, properly administer the sacraments according to
+        Christ’s instructions, and live in obedience to all that Christ commands. A local church is a body of
+        believers formally organized on gospel principles, meeting regularly for the purposes of evangelism,
+        nurture, fellowship, and worship. The Independent Methodist Church is a church consisting of those
+        members within the local church who, as members of the body of Christ, hold the faith set forth in these
+        Articles of Religion and acknowledge the ecclesiastical authority of its governing bodies.
+        </p>
+
+        <h4 id="Sacraments">18. The Sacraments: Baptism and the Lord’s Supper</h4>
+        <p>We believe that water baptism and the Lord’s Supper are the sacraments of the church commanded by
+            Christ and ordained as a means of grace when received through faith. They are tokens of our profession
+            of Christian faith and signs of God’s gracious ministry toward us. By them, He works within us to
+            quicken, strengthen, and confirm our faith.
+            We believe that water baptism is a sacrament of the church, commanded by our Lord and administered
+            to believers. It is a symbol of the new covenant of grace and signifies acceptance of the benefits of the
+            atonement of Jesus Christ. By means of this sacrament, believers declare their faith in Jesus Christ as
+            Savior.
+            We believe that the Lord’s Supper is a sacrament of our redemption by Christ’s death and of our hope in
+            His victorious return, as well as a sign of the love that Christians have for each other. To such as receive
+            it humbly, with a proper spirit and by faith, the Lord’s Supper is made a means through which God
+            communicates grace to the heart.</p>
+        <h4 id="SecondComing">19. The Second Coming of Christ</h4>
+        <p>
+        We believe that the certainty of the personal and imminent return of Christ inspires holy living and zeal
+        for the evangelization of the world. At His return He will fulfill all prophecies made concerning His final
+        and complete triumph over evil.
+        </p>
+
+        <h4 id="Resurrection">20. The Resurrection of the Dead</h4>
+        <p>
+        We believe in the bodily resurrection from the dead of all people— of the just unto the resurrection of
+        life, and of the unjust unto the resurrection of damnation. The resurrection of Christ is the guarantee of
+        the resurrection which will occur at Christ’s Second Coming. The raised body will be a spiritual body, but
+        the person will be whole and identifiable.
+        </p>
+
+        <h4 id="Judgment">21. The Judgment of All Persons</h4>
+        <p>
+        We believe that the Scriptures reveal God as the Judge of all and the acts of His judgment are based on
+        His omniscience and eternal justice. His administration of judgment will culminate in the final meeting of
+        all persons before His throne of great majesty and power, where records will be examined and final
+        rewards and punishments will be administered.
+        </p>
+
+        <h4 id="Destiny">22. Destiny</h4>
+        <p>
+        We believe that the Scriptures clearly teach that there is a conscious personal existence after death. The
+        final destiny of each person is determined by God’s grace and that person’s response, evidenced inevitably by a moral character which results from that individual’s personal and volitional choices and
+        not from any arbitrary decree of God. Heaven with its eternal glory and the blessedness of Christ’s
+        presence is the final abode of those who choose the salvation which God provides through Jesus Christ,
+        but hell with its everlasting misery and separation from God is the final abode of those who neglect this
+        great salvation.
+        </p>
+        </>
+
+
+    )
+
+}
+
+export default FaithConfession;
